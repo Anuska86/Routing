81ae62fbb74187e685db134c5f5675c56a9e7fb8
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-12 10:24:46 +0200

    about

--- routing/src/main.jsx
+++ routing/src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes></Routes>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   </BrowserRouter>
 );
