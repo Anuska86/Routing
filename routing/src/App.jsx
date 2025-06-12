7d83404acbbd6e2c43847005a6639aa50b2ea863
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-12 10:39:43 +0200

    Link

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,5 +1,19 @@
 import "../src/index.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./components/About.jsx";
+import Home from "./components/Home.jsx";
 
 export default function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
